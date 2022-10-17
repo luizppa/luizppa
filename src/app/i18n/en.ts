32942784e4dbee6c9f52d4828d349c5d92eea53e
luizppa <luizphilippep@gmail.com>
2022-10-17 17:53:21 -0300

    Updating old info

--- src/app/i18n/en.ts
+++ src/app/i18n/en.ts
@@ -1,38 +1,38 @@
 export const en = {
-    translation: {
-        header: 'Hi, I\'m',
-        github: {
-            repos: 'Repositories',
-            following: 'Following',
-            followers: 'Followers',
-            issues: 'Issues',
-            stars: 'Stars',
-            forks: 'Forks',
-        },
-        spotify: {
-            playlist_header: 'I recommend that you listen to'
-        },
-        about: {
-            title: 'About',
-            subtitle: 'If you wanna know more about me',
-        },
-        projects: {
-            title: 'Projects',
-            subtitle: 'Here are a few projects I had fun working on',
-        },
-        experiences: {
-            title: 'Experiences',
-            subtitle: 'This is what I\'ve been doing for the past years',
-        },
-        education: {
-            title: 'Education',
-            subtitle: 'And this is where I learned the basis of what I know',
-        },
-        footer: {
-            contact_title: 'If you read this far, you may wish to get in contact',
-            disclaimer: 'Disclaimer: I\'m not a real ninja',
-            disclaimer_far: '...or am I?',
-            footer_bottom: 'made with ♥ and JS, as usual',
-        },
-    }
-}
+  translation: {
+    header: "Hi, I'm",
+    github: {
+      repos: 'Repositories',
+      following: 'Following',
+      followers: 'Followers',
+      issues: 'Issues',
+      stars: 'Stars',
+      forks: 'Forks',
+    },
+    spotify: {
+      playlist_header: 'I recommend that you listen to',
+    },
+    about: {
+      title: 'About',
+      subtitle: 'If you wanna know more about me',
+    },
+    projects: {
+      title: 'Projects',
+      subtitle: 'Here are a few projects I had fun working on',
+    },
+    experiences: {
+      title: 'Experiences',
+      subtitle: '',
+    },
+    education: {
+      title: 'Education',
+      subtitle: '',
+    },
+    footer: {
+      contact_title: 'If you read this far, you might wish to get in contact',
+      disclaimer: "Disclaimer: I'm not a real ninja",
+      disclaimer_far: '...or am I?',
+      footer_bottom: 'made with ♥ and JS, as usual',
+    },
+  },
+};
--- src/app/i18n/pt.ts
+++ src/app/i18n/pt.ts
@@ -1,38 +1,38 @@
 export const pt = {
-    translation: {
-        header: 'Oi, meu nome é',
-        github: {
-            repos: 'Repositórios',
-            following: 'Seguindo',
-            followers: 'Seguidores',
-            issues: 'Issues',
-            stars: 'Estrelas',
-            forks: 'Forks',
-        },
-        spotify: {
-            playlist_header: 'Minha recomendação pra você'
-        },
-        about: {
-            title: 'Sobre mim',
-            subtitle: 'Aqui estão algumas coisas que eu gosto',
-        },
-        projects: {
-            title: 'Projetos',
-            subtitle: 'Esses são alguns projetos dos quais gostei de participar',
-        },
-        experiences: {
-            title: 'Experiências',
-            subtitle: 'Isso é o que eu tenho feito nos último anos',
-        },
-        education: {
-            title: 'Educação',
-            subtitle: 'E esses foram os lugares onde aprendi as bases do que sei',
-        },
-        footer: {
-            contact_title: 'Se você leu até aqui, talvez queira entrar em contato',
-            disclaimer: 'Atenção: não sou um ninja de verdade',
-            disclaimer_far: '...ou será que sou?',
-            footer_bottom: 'feito com ♥ e JS',
-        },
-    }
-}
+  translation: {
+    header: 'Oi, meu nome é',
+    github: {
+      repos: 'Repositórios',
+      following: 'Seguindo',
+      followers: 'Seguidores',
+      issues: 'Issues',
+      stars: 'Estrelas',
+      forks: 'Forks',
+    },
+    spotify: {
+      playlist_header: 'Minha recomendação pra você',
+    },
+    about: {
+      title: 'Sobre mim',
+      subtitle: 'Aqui estão algumas coisas que eu gosto',
+    },
+    projects: {
+      title: 'Projetos',
+      subtitle: 'Esses são alguns projetos dos quais gostei de participar',
+    },
+    experiences: {
+      title: 'Experiências',
+      subtitle: '',
+    },
+    education: {
+      title: 'Educação',
+      subtitle: '',
+    },
+    footer: {
+      contact_title: 'Se você leu até aqui, talvez queira entrar em contato',
+      disclaimer: 'Atenção: não sou um ninja de verdade',
+      disclaimer_far: '...ou será que sou?',
+      footer_bottom: 'feito com ♥ e JS',
+    },
+  },
+};
